49ac6cfd8e27bf36598b3697afb138d2119b624c
wuy <1311695042@qq.com>
2021-12-31 18:36:01 +0800

    * 优化登陆、首页、个人中心

--- pages/personal/personal.vue
+++ pages/personal/personal.vue
@@ -1,46 +1,22 @@
 <template>
-	<view>
-		<u-navbar :is-back="false" title="　" :border-bottom="false">
-			<view class="u-flex u-row-right" style="width: 100%;">
-				<view class="camera u-flex u-row-center">
-					<u-icon name="camera-fill" color="#000000" size="48"></u-icon>
-				</view>
+	<view class="personal page-bg-color">
+		<view class="personal-header">
+			<u-avatar :src="require('@/static/logo.png')" size="large"></u-avatar>
+			<view class="personal-header-info">
+				<text class="personal-header-info-name">{{userInfo.displayName || '-'}}</text>
+				<text class="u-tips-color">{{userInfo.displayName || '-'}}</text>
 			</view>
-		</u-navbar>
-		<view class="u-flex user-box u-p-l-30 u-p-r-20 u-p-b-30">
-			<view class="u-m-r-10">
-				<u-avatar :src="member.avatarUrl" size="140"></u-avatar>
-			</view>
-			<view class="u-flex-1">
-				<view class="u-font-18 u-p-b-20">{{member.nickname}}</view>
-				<view class="u-font-14 u-tips-color">{{member.gender}}</view>
-			</view>
-			<view class="u-m-l-10 u-p-10">
-				<u-icon name="scan" color="#969799" size="28"></u-icon>
-			</view>
-			<view class="u-m-l-10 u-p-10">
-				<u-icon name="arrow-right" color="#969799" size="28"></u-icon>
-			</view>
+			<u-icon size="40" name="edit-pen"></u-icon>
 		</view>
-		
 		<view class="u-m-t-20">
 			<u-cell-group>
-				<u-cell-item icon="rmb-circle" title="支付"></u-cell-item>
+				<u-cell-item icon="weixin-fill" title="绑定微信" @click="weiBind"></u-cell-item>
 			</u-cell-group>
 		</view>
-		
-		<view class="u-m-t-20">
-			<u-cell-group>
-				<u-cell-item icon="star" title="收藏"></u-cell-item>
-				<u-cell-item icon="heart" title="关注"></u-cell-item>
-			</u-cell-group>
+		<view class="u-m-t-80">
+			<u-button type="error" @click="logout">退出登录</u-button>
 		</view>
-		
-		<view class="u-m-t-20">
-			<u-cell-group>
-				<u-cell-item icon="setting" title="设置"></u-cell-item>
-			</u-cell-group>
-		</view>
+		<u-top-tips ref="uTips"></u-top-tips>
 	</view>
 </template>
 
@@ -48,33 +24,66 @@
 	export default {
 		data() {
 			return {
-				member: JSON.parse(uni.getStorageSync('member') || '{}') ,
-				show:true
+				userInfo: JSON.parse(uni.getStorageSync('userInfo') || '{}')
 			}
 		},
-		onLoad() {
-			
-		},
 		methods: {
-			
-		}
+			logout() {
+				const that = this
+				that.$dibootApi.post('/h5/logout').then(() => {
+					that.$tip(that.$refs.uTips, '退出成功!').then(() => {
+						uni.clearStorageSync()
+						let timer = setTimeout(() => {
+							clearTimeout(timer)
+						      uni.reLaunch({
+						      	url: '/pages/login/index'
+						      });
+						  }, 0)
+						
+					})
+				})
+			},
+			//
+			/**
+			 * 微信绑定
+			 */
+			weiBind() {
+				// 小程序登陆
+				// #ifdef MP-WEIXIN
+				this.$miniLogin.setBindWx(true).setTip(this.$refs.uToast).setUrlPath('/pages/personal/personal').go()
+				//#endif
+				// 微信公众号登陆
+				// #ifdef H5
+				this.$mpLogin.redirect(true)
+				//#endif
+			}
+		},
 	}
 </script>
 
-<style lang="scss">
-page{
-	background-color: #ededed;
-}
+<style scoped lang="scss">
+	.personal {
+		padding: 24rpx 24rpx 0;
 
-.camera{
-	width: 54px;
-	height: 44px;
-	
-	&:active{
-		background-color: #ededed;
+		&-header {
+			display: flex;
+			background-color: #fff;
+			padding: 40rpx;
+			border-radius: 20rpx;
+			margin-bottom: 40rpx;
+
+			&-info {
+				flex: 1;
+				display: flex;
+				flex-direction: column;
+				justify-content: space-around;
+				margin-left: 40rpx;
+
+				&-name {
+					font-weight: bold;
+					font-size: 32rpx;
+				}
+			}
+		}
 	}
-}
-.user-box{
-	background-color: #fff;
-}
 </style>
